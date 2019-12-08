c3cd7cdda137e99413647449cc10a4e8be3b3dc3
Lionel Bersee <lionel1232@gmail.com>
2019-12-08 13:54:11 +1100

    fix incorrect test

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,6 +1,6 @@
 import pytest
 
-from toy_robot_challenge.positioning import Direction, Position
+from toy_robot_challenge.positioning import Position
 from toy_robot_challenge.table import Table
 
 
@@ -14,16 +14,16 @@ def test_create_table():
 
 
 @pytest.mark.parametrize("x, y", [(-1, 0), (0, -1), (-1, -1), (5, 5), (6, 5), (5, 6)])
-def test_valid_position_returns_false_for_invalid_position(x, y):
+def test_valid_position_returns_false_for_invalid_position(x, y, mocker):
     table = Table(5, 5)
-    position = Position(x, y, Direction.NORTH)
+    position = Position(x, y, mocker.Mock())
 
     assert table.valid_position(position) is False
 
 
 @pytest.mark.parametrize("x, y", [(0, 0), (4, 4), (3, 3)])
-def test_valid_position_returns_true_for_valid_position(x, y):
+def test_valid_position_returns_true_for_valid_position(x, y, mocker):
     table = Table(5, 5)
-    position = Position(x, y, Direction.NORTH)
+    position = Position(x, y, mocker.Mock())
 
     assert table.valid_position(position) is True
